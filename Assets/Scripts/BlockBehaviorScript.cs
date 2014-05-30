649c39e119c1c2460222086e1b196a936ada1204
Martin Rohwedder <m.rohwedder@gmail.com>
2014-05-30 20:54:11 +0200

    Player movement and Block Control added (Not yet perfected)

---
+++ Assets/Scripts/BlockBehaviorScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockBehaviorScript : MonoBehaviour {
+
+	private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+		newPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime);
+	}
+
+	public void changePosition(Vector3 to) {
+		newPosition = to;
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviorScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerBehaviorScript : MonoBehaviour {
+
+	public float moveSpeed = 8.0f;
+	public float turnSpeed = 140.0f;
+
+	private CharacterController characterController;
+
+	void Start() {
+		characterController = gameObject.GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		MovePlayer();
+		RotatePlayer();
+	}
+
+	/**
+	 * Move the player around with the arrow keys (up or down) or w and s.
+	 */
+	void MovePlayer() {
+		characterController.Move(Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * moveSpeed * Time.deltaTime);
+	}
+
+	/**
+	 * Rotate the player around with the arrow keys (left and right) or d and a.
+	 */
+	void RotatePlayer() {
+		transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0));
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			Vector3 newPosition = new Vector3(hit.gameObject.transform.position.x, hit.gameObject.transform.position.y, hit.gameObject.transform.position.z + 4);
+			hit.gameObject.GetComponent<BlockBehaviorScript>().changePosition(newPosition);
+		}
+	}
+}
