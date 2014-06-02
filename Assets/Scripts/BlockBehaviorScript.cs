392b868cc2f50c507e87382c6053e10d93050f9d
Martin Rohwedder <m.rohwedder@gmail.com>
2014-06-02 08:28:51 +0200

    wall test asset added

--- Assets/Scripts/BlockBehaviorScript.cs
+++ Assets/Scripts/BlockBehaviorScript.cs
@@ -1,30 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * This scripts will handle the behavior of the block.
+ * A block can slide in either the x or the z directions.
+ * Blocks however can't slide through other blocks or
+ * solid properties (walls, tables and such)
+ * 
+ * @author Martin Rohwedder
+ * @version 1.0
+ */
 public class BlockBehaviorScript : MonoBehaviour {
 
-	private Vector3 newPosition;
+	public float speed = 2f;   //Speed of the slide
+
+	private const KeyCode PUSH_KEY = KeyCode.P;
+	private Vector3 newPosition;   //The new position, where the Block will slide to.
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		newPosition = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * 2f);
+	void Update ()
+	{
+		transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
 	}
 
 	/**
 	 * Set the new position of the cube
 	 */
-	public void ChangePosition(Vector3 to) {
+	void ChangePosition(Vector3 to)
+	{
 		newPosition = to;
 	}
 
-	void OnTriggerStay(Collider col) {
-		Debug.Log("Trigger Event fired");
-
+	/**
+	 * Is fired when the player is colliding with the block
+	 */
+	void OnTriggerStay(Collider col)
+	{
+		//Calculate the direction to push the Block
 		Vector3 direction = transform.position - col.gameObject.transform.position;
 		
 		direction.y = 0f;
@@ -40,22 +58,33 @@ public class BlockBehaviorScript : MonoBehaviour {
 		
 		direction.Normalize();
 
+		//Push the Block in the calculated direction, if player presses the Push key and the block can slide
+		if (Input.GetKeyDown(PUSH_KEY) && CanSlide(direction))
+		{
+			ChangePosition(transform.position + direction * 3f);
+		}
+	}
+
+	/**
+	 * False is returned, if the block will hit another block or a solid property (walls and such)
+	 */
+	bool CanSlide(Vector3 direction)
+	{
 		bool canSlide = true;
+		float length = 4f;
+
 		RaycastHit rayHit;
 		Ray ray = new Ray(transform.position, direction);
-		Debug.DrawRay(transform.position, direction * 4.5f);
-
-		if (Physics.Raycast(ray, out rayHit, 4.5f))
+		Debug.DrawRay(transform.position, direction * length);
+		
+		if (Physics.Raycast(ray, out rayHit, length))
 		{
-			if (rayHit.collider.tag == "Block")
+			if (rayHit.collider.tag == "Block" || rayHit.collider.tag == "SolidProperty")
 			{
 				canSlide = false;
 			}
 		}
 
-		if (Input.GetKeyDown(KeyCode.P) && canSlide)
-		{
-			ChangePosition(transform.position + direction * 3f);
-		}
+		return canSlide;
 	}
 }
